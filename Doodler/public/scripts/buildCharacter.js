adede8bb3d7154a98e8f61c8d7d716e26cb79a3a
Sammy Consani <sammyconsani@Sammys-MacBook-Pro.local>
2020-05-06 17:36:58 -0700

    one version of toggle is working just for the head choices

--- Doodler/public/scripts/buildCharacter.js
+++ Doodler/public/scripts/buildCharacter.js
@@ -32,51 +32,86 @@ $(document).ready(function(){
 		console.log("renderBuilderParts");
 
 		for (i=0; i<parts.length; i++ ) {
-			$("#body-parts").append(`<div class = "body-btn" data-target = "acc" id = "${parts[i]}"><span><p>${parts[i]}</p></span></div>`)
+			$("#body-parts").append(`<div class = "body-btn" data-target = "acc" id = "${parts[i]}"><span><p class ="">${parts[i]}</p></span></div>`)
 
 			};
 	};
 
 renderBuilderParts();
 
-	function headonClickShow() {
+								//THIS IS WORKING
+function headonClickShow() {
 
-		// if data-target = "head" { then do this and set it to show, if a different one is 
-		// 	clicked- then hide the previous and show the new one}
-		console.log("headonClickShow- FUNCTION");
+	function toggleHeads() {
+		var x = document.getElementById("options-row-heads")
+		if (x.style.display ==="none") {
+			x.style.display = "block";
+		}
+		else {
+			x.style.display = "none";
+		}
+	}
 
-		$('#head').on("click", function(event){
-			for (i=0; i<headsthumb.length; i++){
-			$("#options-row").append(`<div class="box col-6 col-sm-3">
-									<img class = "thumb mt-2 mb-2" src ="${headsthumb[i]}" id = "${headsthumb[i]} alt = "" height = "100">
-					 				</div> `)
+// 		// if data-target = "head" { then do this and set it to show, if a different one is 
+// 		// 	clicked- then hide the previous and show the new one}
+// 		console.log("headonClickShow- FUNCTION");
 
-			// let heads = "live"
-			};
-		});
+	$('#head').on("click", function(event){
+		toggleHeads();
+	});
 	};
-// let headsactive = "live"
 
 headonClickShow();
 
-	function headClicked() {
-
-		// console.log("headClicked- FUNCTION");
-
-		// if (heads == "live") {
-		// 	console.log(headonClickShow)
-			$(`#${headsthumb[i]}`).on("click", function (event){
-				$("dooodle-container").append(`<div class = "head-image-active">
-												<img class = "thumb" src = ${headsthumb[i]} alt ="testing head show" height = "100">
-												</div>`)
-																});
-												}
-		// else {
-		// 	console.log("this isn't working")
-		// }
-
-							// };
-headClicked();
-
 });
+							//THIS ABOVE IS WORKING
 
+							///the below is an attempt for the options for loop
+
+///this was a function below to print out the options for the heads using a for loop
+///i was trying to figure out how to use a toggle and also use a for loop. 
+
+// 			for (i=0; i<headsthumb.length; i++){
+// 			$("#options-row").append(`<div class="box col-6 col-sm-3">
+// 									<img class = "thumb mt-2 mb-2" src ="${headsthumb[i]}" id = "${headsthumb[i]} alt = "" height = "100">
+// 					 				</div> `)
+
+// 			};
+// 		});
+// 	};
+// 
+								//ATTEMPT - multiple variables in one function
+								// to do the same thing with toggling for each one
+								// depending on what is active or not
+
+// function bodypartClickShow() {
+
+// 	function toggleChoices() {
+// 		var h = document.getElementById("options-row-heads")
+// 		if (h.style.display ==="none") {
+// 			h.style.display = "block";
+// 		}
+// 		else {
+// 			h.style.display = "none";
+// 		}
+// 	}
+
+// 		// if data-target = "head" { then do this and set it to show, if a different one is 
+// 		// 	clicked- then hide the previous and show the new one}
+// 		console.log("headonClickShow- FUNCTION");
+
+	// $('#head').on("click", function(event){
+	// 	toggleChoices();
+	// });
+
+
+	// $('#torso').on("click", function(event){
+	// var t = document.getElementById("options-row-torsos")
+	// 	if(h.style.display ==="block"){
+	// 		h.style.display = "none"
+	// 		t.style.display = "block"
+	// 	}
+	// })
+	// };
+
+// bodypartClickShow();
