975898727eedbc957c83fd4b5c59bc935664d0df
Sammy Consani <sammyconsani@Sammys-MBP.attlocal.net>
2020-05-07 16:54:53 -0700

    still trying

--- Doodler/public/scripts/createCharacter.js
+++ Doodler/public/scripts/createCharacter.js
@@ -3,47 +3,132 @@ console.log("sanity check- create a character")
 
 $(document).ready(function(){
 
-	$(function(){
-		console.log("running jquery functinon")
+// function showhide() {
+//     $('#showdiv1').on("click", function(event) {
+//         console.log("click is working")
+//         $('div[id^=div]').hide();
+//         $('#div1').show();
+//     });
+//     $('#showdiv2').on("click", function(event) {
+//         $('div[id^=div]').hide();
+//         $('#div2').show();
+//     });
 
-		$('#showall').on("click", function(event){
-			console.log("running showall functinon")
-			$('.targetDiv').show();
-		});
+//     $('#showdiv3').on("click",function(event) {
+//         $('div[id^=div]').hide();
+//         $('#div3').show();
+//     });
 
-		$('.showSingle').on("click", function(event){
-			console.log("running show single functinon")
+    // $('#showdiv4').click(function() {
+    //     $('div[id^=div]').hide();
+    //     $('#div4').show();
+    // });
 
-			$('targetDiv').hide();
-			console.log("hide is working")
+    // $('#showdiv5').click(function() {
+    //     $('div[id^=div]').hide();
+    //     $('#div5').show();
+    // });
 
-			$('#div' +$(this).attr('target')).show();
-			console.log("this div is working")
-			console.log($(this))
-			
-		});
-	});
+    // $('#showdiv6').click(function() {
+    //     $('div[id^=div]').hide();
+    //     $('#div6').show();
+    // });
+
+// };
+
+// showhide();
+// });
 
 
-	
 
-	// function showFeatures(){
-	// 	console.log("running new functinon")
-	// 	$('#showall').on("click", function(event){
-	// 		console.log("running new functinon")
-	// 		$('.targetDiv').show();
-	// 	});
+	// THIS WORKS
+	let headsElements = document.getElementById('heads');
+	let torsoElements = document.getElementById('torsos');
 
-	// 	$('.showSingle').on("click", function(event){
-	// 		console.log("running new functinon")
-	// 		$('targetDiv').hide();
-	// 		console.log("hide is working")
-	// 		$('#div' +$(this).attr('target')).show();
-	// 	});
-	// };
+// THIS WORKS
+function showElements() {
+	// let headsElements = document.getElementById('heads');
+	document.querySelector('.head1').addEventListener('click', handleHead)
+	document.querySelector('.torso2').addEventListener('click', handleTorso)
+	// document.querySelector('.legs3').addEventListener('click', handleLeg)
 
-	// showFeatures();
+		let headContent = document.querySelector('.heads')
+		let torsoContent = document.querySelector('.torsos')
+		// let legContent = document.querySelector('.legs')
+// THIS WORKS
+	function handleHead(event){
+		if (headContent.style.display === "none") {
+			headContent.style.display = "block"
+			handleTorso();
 
+		} else {
+			headContent.style.display = "none"
+		}
+
+}
+handleHead();
+// THIS WORKS
+	function handleTorso(event) {
+	if (torsoContent.style.display === "none") {
+			torsoContent.style.display = "block"
+
+		} else {
+			torsoContent.style.display = "none"
+		}
+
+
+	}
+handleTorso();
+};
+// THIS WORKS
+showElements();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//THIS DOESNT WORK BUT IT MAKES SENSE
+
+// var divs = ["all", "heads", "torsos", "legs", "feet", "backgrounds", "accessories"];
+// var visibleDivId = null;
+
+// function toggleVisibility(divId) {
+// 	if(visibleDivId === divId) {
+// 		visibleDivID = null;
+// 	} else {
+// 		visibleDivId = divId;
+// 	}
+// 	hideNonVisibleDivs();
+// }
+// function hideNonVisibleDivs() {
+// 	var i, divId, div;
+// 	for (i = 0; i <divs.length; i++) {
+// 		divId = divs[i];
+// 		div = document.getElementById(divId);
+// 		if(visibleDivId === divId) {
+// 			div.style.display = "block";
+// 		} else {
+// 			div.style.display = "none";
+// 		}
+// 	}
+// }
+
+// toggleVisibilty(divId);
+
+
 
 
