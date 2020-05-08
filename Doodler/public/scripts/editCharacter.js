bebfc71efffee6c415d5047af92198e382179284
Sammy Consani <sammyconsani@Sammys-MacBook-Pro.local>
2020-05-08 08:52:39 -0700

    centered head

---
+++ Doodler/public/scripts/editCharacter.js
@@ -0,0 +1,19 @@
+console.log("testing edit character.js")
+
+
+
+
+
+
+
+// works but not dynamic
+$('#redhead').click(function() {
+	$('#head1').append(`<img src = "https://i.imgur.com/thqi8Jj.png" class = "char-head">`)
+	console.log("running click")
+
+});
+
+
+
+
+
