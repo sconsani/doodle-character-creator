9a1a2372fd776ef76f031c2462c52f49f55486d7
Sammy Consani <sammyconsani@Sammys-MacBook-Pro.local>
2020-05-08 12:48:51 -0700

    head, torso, legs working in and out of order BUT NOT ALIGNED

--- Doodler/public/scripts/editCharacter.js
+++ Doodler/public/scripts/editCharacter.js
@@ -1,7 +1,8 @@
 console.log("testing edit character.js")
 
-let divone = document.getElementById('div1')
+				////////DIV ONE	DIV ONE	DIV ONE	DIV ONE	DIV ONE ///////
 
+let divone = document.getElementById('div1')
 
 function clearcontent() {
 	console.log("clearcontent")
@@ -57,3 +58,85 @@ divone.onclick = function(event) {
 	appendNewHead(targetid);
 };
 
+					////////DIV TWO	DIV TWO	DIV TWO	DIV TWO	DIV TWO ///////
+
+let divtwo = document.getElementById('div2')
+console.log(divtwo)
+
+
+function cleartorso() {
+	console.log("clearcontent")
+	$('#torso1').empty();
+
+};
+
+function appendNewTorso(targetid) {
+	if(event.target.id === "blackshirt") {
+		console.log("clearing content again")
+		cleartorso();
+		$('#torso1').append(`<img src="https://i.imgur.com/3QSSCSw.png" class = "build-img">`)
+
+	} 
+	
+
+	else {
+		console.log("add more images")
+	}
+
+	console.log("append new head")
+};
+
+divtwo.onclick = function(event) {
+	console.log(" on click")
+	let target = event.target;
+	let targetid = event.target.id
+	console.log(event.target)
+	console.log(event.target.id)
+
+	if (target.tagName = 'img'){
+	cleartorso();
+}
+	console.log("clear before appending")
+	appendNewTorso(targetid);
+};
+
+			////////DIV THREE	DIV THREE	DIV THREE	DIV THREE	DIV THREE ///////
+
+let divthree = document.getElementById('div3')
+console.log(divthree)
+
+
+function clearlegs() {
+	console.log("clearcontent")
+	$('#legs1').empty();
+
+};
+
+function appendNewLegs(targetid) {
+	if(event.target.id === "greenpants") {
+		console.log("clearing content again")
+		clearlegs();
+		$('#legs1').append(`<img src="https://i.imgur.com/QKo9vDT.png" class = "build-img">`)
+
+	} 
+	
+	else {
+		console.log("add more images")
+	}
+
+	console.log("append new legs")
+};
+
+divthree.onclick = function(event) {
+	console.log(" on click")
+	let target = event.target;
+	let targetid = event.target.id
+	console.log(event.target)
+	console.log(event.target.id)
+
+	if (target.tagName = 'img'){
+	clearlegs();
+}
+	console.log("clear before appending")
+	appendNewLegs(targetid);
+};
