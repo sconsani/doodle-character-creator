4cd07bb0e9c807f68a27201d338cda07fd956b45
Sammy Consani <sammyconsani@Sammys-MacBook-Pro.local>
2020-05-11 00:39:12 -0700

    attempting deploy

--- Doodler/models/index.js
+++ Doodler/models/index.js
@@ -3,16 +3,16 @@ const Character = require('./character');
 exports.Character = Character;
 const CharacterModel = require('./character');
 
-
-mongoose.connect('mongodb://localhost:27017/doodler', {
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-})
-	.then(()=> console.log("MongoDB connected successfully"))
-	.catch((err)=> console.log(`MongoDB error: ${err}`))
-
+// mongoose.connect('mongodb://localhost:27017/doodler', {
+// mongoose.connect('mongodb://localhost:27017/doodler', {
+// 	useNewUrlParser: true,
+// 	useFindAndModify: false,
+// 	useCreateIndex: true,
+// 	useUnifiedTopology: true,
+// })
+// 	.then(()=> console.log("MongoDB connected successfully"))
+// 	.catch((err)=> console.log(`MongoDB error: ${err}`))
+mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
 
 module.exports = {
 	Character : CharacterModel
